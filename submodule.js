ebd38fcc3a16c9041e7fae02bfe8bfc69d719662
Junyup <junyup@techtaka.com>
2021-10-07 11:30:29 +0900

    sub module update

--- submodule.js
+++ submodule.js
@@ -1,9 +1,7 @@
-export const log = (message) => {
+export const log = message => {
   console.log('[submodule]'+ message);
 }
 
-const add = (a, b) => {
-  return a + b;
-}
+const add = (...args) => args.reduce((acc, cur) => acc + cur, 0);
 
 export { add };
